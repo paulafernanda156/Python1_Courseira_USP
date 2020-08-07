af71334efcb43c7d5c1eb78e111a9ccb5ada94a0
Paula Monteiro <monteiromelo.paula@gmail.com>
2020-08-07 05:22:13 -0300

    Ex. 1 - Cousera, Python 1.

---
+++ exe1.py
@@ -0,0 +1,7 @@
+lado = int(input("Digite o valor correspondente ao lado de um quadrado: " ))
+
+perímetro = (lado * 4)
+
+área = (lado * lado)
+
+print ( 'perímetro: ', perímetro, '-', 'área:', área )
