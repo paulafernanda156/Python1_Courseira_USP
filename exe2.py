e484c1a1a320ee962b71c8b34d55f91b280e953b
Paula Monteiro <monteiromelo.paula@gmail.com>
2020-08-07 06:36:24 -0300

    Coursera, teste 2

---
+++ exe2.py
@@ -0,0 +1,11 @@
+A = float( input("Digite a primeira nota: " ) )
+
+B = float( input("Digite a segunda nota: " ) )
+
+C = float( input("Digite a terceira nota: " ) )
+
+D = float( input("Digite a quarta nota: " ) )
+
+média = ( A + B + C + D ) / 4
+
+print ( 'A média aritmética é', média )
